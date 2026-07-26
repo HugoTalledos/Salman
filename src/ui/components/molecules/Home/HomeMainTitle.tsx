758e8ee6c8509e730069766d2cc3f6bdb6c6c5ab
Hugo Talledos <hugo.talledos05@gmail.com>
2026-07-26 15:24:28 -0500

    feat(ui): create HomeMainTitle component with header and paragraph elements

---
+++ src/ui/components/molecules/Home/HomeMainTitle.tsx
@@ -0,0 +1,9 @@
+import './home.css'
+export function HomeMainTitle () {
+  return (
+    <header className="inicio-cabecera">
+      <h1>Salman</h1>
+      <p>Diseña, construye y compila tus clases.</p>
+    </header>
+  )
+}
