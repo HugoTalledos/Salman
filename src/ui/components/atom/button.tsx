d510f6fc2896e35686dedd806673b08ac082914a
Hugo Talledos <hugo.talledos05@gmail.com>
2026-07-28 17:10:28 -0500

    feat(ui): update BaseButton component to accept className and ariaLabel props for enhanced accessibility and styling

--- src/ui/components/atom/button.tsx
+++ src/ui/components/atom/button.tsx
@@ -1,14 +1,23 @@
 interface BaseButtonProps {
   label: string
-  onClickBtn: () => void
+  onClickBtn: (event: React.MouseEvent<HTMLButtonElement>) => void
+  className?: string
+  ariaLabel?: string
 }
 
 export function BaseButton ({
   label,
-  onClickBtn
+  onClickBtn,
+  className,
+  ariaLabel,
 }: BaseButtonProps) {
   return (
-    <button type="button" onClick={onClickBtn}>
+    <button
+      type="button"
+      className={className}
+      aria-label={ariaLabel}
+      onClick={onClickBtn}
+    >
       { label }
     </button>
   )
